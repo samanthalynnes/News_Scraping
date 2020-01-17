acf6fa3fae3dada2515c517c185ff8e58f0de06a
Samantha Lynnes <samanthalynnes522@gmailcom>
2020-01-17 16:08:58 -0600

    added models and app.js code

--- models/Article.js
+++ models/Article.js
@@ -16,10 +16,18 @@ var ArticleSchema = new Schema({
         type: String,
         required: true
     },
+    saved: {
+        type: Boolean,
+        default: true
+    },
+    descrip: {
+        type: String,
+        require: false
+    },
     // `note` is an object that stores a Note id
     // The ref property links the ObjectId to the Note model
     // This allows us to populate the Article with an associated Note
-    note: {
+    notes: {
         type: Schema.Types.ObjectId,
         ref: "Note"
     }
---
+++ public/js/app.js
@@ -0,0 +1,70 @@
+
+// When someone clicks Scrape button
+$("#scrape").on("click", function () {
+    $.ajax({
+        method: "GET",
+        url: "/scrape",
+    }).done(function (data) {
+        console.log(data)
+        window.location = "/"
+    })
+});
+
+// Save article button click
+$(".save").on("click", function () {
+    var thisId = $(this).attr("data-id");
+    $ajax({
+        method: "POST",
+        url: "/articles/save/" + thisId
+    }).done(function (data) {
+        windown.location = "/"
+    })
+});
+
+//  Delete article button click
+$(".delete").on("click", function () {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "POST",
+        url: "/articles/delete/" + thisId
+    }).done(function (data) {
+        window.location = "/saved"
+    })
+});
+
+//  When someone saves a note
+$(".saveNote").on("click", function () {
+    var thisId = $(this).attr("data-id");
+    if (!$("#noteText" + thisId).val()) {
+        alert("please enter a note to save")
+    } else {
+        $.ajax({
+            method: "POST",
+            url: "/notes/save/" + thisId,
+            data: {
+                text: $("#noteText" + thisId).val()
+            }
+        }).done(function (data) {
+            // Log the response
+            console.log(data);
+            // Empty the notes section
+            $("#noteText" + thisId).val("");
+            $(".modalNote").modal("hide");
+            window.location = "/saved"
+        });
+    }
+});
+
+//  When someone deletes a note
+$(".deleteNote").on("click", function () {
+    var noteId = $(this).attr("data-note-id");
+    var articleId = $(this).attr("data-article-id");
+    $.ajax({
+        method: "DELETE",
+        url: "/notes/delete/" + noteId + "/" + articleId
+    }).done(function (data) {
+        console.log(data)
+        $(".modalNote").modal("hide");
+        window.location = "/saved"
+    })
+});
